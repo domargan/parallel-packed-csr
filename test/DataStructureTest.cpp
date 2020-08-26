567835c01378356a3f96205a706a1116ced6b93a
Christian Menges <christian.menges@tum.de>
2020-08-26 17:38:20 +0200

    Add test case with 1E4 edges and check locking

--- test/DataStructureTest.cpp
+++ test/DataStructureTest.cpp
@@ -16,22 +16,23 @@ TEST_F(DataStructureTest, add_node) {
   EXPECT_EQ(pcsr.get_n(), 0);
   pcsr.add_node();
   EXPECT_EQ(pcsr.get_n(), 1);
+  EXPECT_EQ(pcsr.get_neighbourhood(0).size(), 0);
 }
 
 TEST_F(DataStructureTest, add_edge) {
-  PPPCSR pcsr(0, 0, false, 1);
+  PPPCSR pcsr(10, 10, false, 1);
   // Try to add edge without corresponding node
-  pcsr.add_edge(0, 1, 1);
-  pcsr.add_node();
+  pcsr.add_edge(11, 1, 1);
+
   pcsr.add_edge(0, 1, 1);
   EXPECT_TRUE(pcsr.edge_exists(0, 1));
   EXPECT_EQ(pcsr.get_neighbourhood(0).size(), 1);
-  EXPECT_EQ(pcsr.get_n(), 1);
+  EXPECT_EQ(pcsr.get_n(), 10);
   EXPECT_EQ(pcsr.get_neighbourhood(2).size(), 0);
 }
 
 TEST_F(DataStructureTest, remove_edge) {
-  PPPCSR pcsr(0, 0, false, 1);
+  PPPCSR pcsr(10, 10, false, 1);
   pcsr.add_node();
   pcsr.remove_edge(0, 1);
   EXPECT_FALSE(pcsr.edge_exists(0, 1));
@@ -44,22 +45,29 @@ TEST_F(DataStructureTest, remove_edge) {
 }
 
 TEST_F(DataStructureTest, add_remove_edge_1E4) {
-    PPPCSR pcsr(0, 0, false, 1);
-    // Try to add edge without corresponding node
-    pcsr.add_node();
-    constexpr int edge_count = 1E4;
-    for(int i = 1; i < edge_count+1; ++i) {
-        pcsr.add_edge(0, i, i);
-        EXPECT_TRUE(pcsr.edge_exists(0, i)) << i;
+  PCSR pcsr(10, 10, false, 0);
+  constexpr int edge_count = 1E4;
+  for (int i = 1; i < edge_count + 1; ++i) {
+    pcsr.add_edge(0, i, i);
+    EXPECT_TRUE(pcsr.edge_exists(0, i)) << i;
+    // Check whether all locks were released
+    for (int j = 0; j < pcsr.edges.N / pcsr.edges.logN; ++j) {
+      EXPECT_TRUE(pcsr.edges.node_locks[j]->lockable())
+          << "Current iteration: " << i << " lock id: " << j;
     }
+  }
+  EXPECT_EQ(pcsr.get_n(), 10);
+  EXPECT_EQ(pcsr.getNode(0).num_neighbors, edge_count);
 
-    EXPECT_EQ(pcsr.get_n(), 1);
-    EXPECT_EQ(pcsr.getNode(0).num_neighbors, edge_count);
-
-    for(int i = 1; i < edge_count+1; ++i) {
-        pcsr.remove_edge(0, i);
-        EXPECT_FALSE(pcsr.edge_exists(0, i)) << i;
+  for (int i = 1; i < edge_count + 1; ++i) {
+    pcsr.remove_edge(0, i);
+    EXPECT_FALSE(pcsr.edge_exists(0, i)) << i;
+    // Check whether all locks were released
+    for (int j = 0; j < pcsr.edges.N / pcsr.edges.logN; ++j) {
+      EXPECT_TRUE(pcsr.edges.node_locks[j]->lockable())
+          << "Current iteration: " << i << " lock id: " << j;
     }
-    EXPECT_EQ(pcsr.get_neighbourhood(0).size(), 0);
-    EXPECT_EQ(pcsr.get_n(), 0);
+  }
+  EXPECT_EQ(pcsr.get_neighbourhood(0).size(), 0);
+  EXPECT_EQ(pcsr.get_n(), 10);
 }
