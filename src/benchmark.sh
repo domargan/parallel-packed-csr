b3fc9ae17f77f56ae043f2c6d8388a40d03655b5
Christian Menges <christian.menges@tum.de>
2020-08-27 00:04:47 +0200

    Add avg calculation to benchmark script

--- src/benchmark.sh
+++ src/benchmark.sh
@@ -2,7 +2,8 @@
 
 # Written by Christian Menges, 10. July 2020
 
-outFile=strong_scaling_results.csv
+time=$(date +"%y_%m_%d_%k:%M:%S")
+outFile="${time}_strong_scaling_results.csv"
 
 repetitions=3
 
@@ -10,26 +11,36 @@ rm -f $outFile
 
 header="threads"
 for ((r = 0; r < repetitions; r++)); do
-  header="${header},instime${r}"
+  header="${header},insertiontime${r}"
 done
+header="${header},insertiontimeAvg"
 
 for ((r = 0; r < repetitions; r++)); do
-  header="${header},deltime${r}"
+  header="${header},deletiontime${r}"
 done
+header="${header},deletiontimeAvg"
+
 echo "$header" | tee $outFile
 
 for i in {0..6}; do
   t=$((2 ** i))
-  line=""
+  insert=""
   for ((r = 0; r < repetitions; r++)); do
     output=$(./parallel-packed-csr -threads=$t -update_file=../data/update_files/insertions.txt | sed '/Elapsed/!d' | sed -n '0~2p' | sed 's/Elapsed wall clock time: //g')
-    line="${line},${output}"
+    insert="${insert},${output}"
   done
+  avg=$(echo "$insert" | awk '{l=split($0,a,","); s=0; for (i in a)s+=a[i]; print s/(l-1);}')
+  insert="${insert},${avg}"
 
+  delete=""
   for ((r = 0; r < repetitions; r++)); do
     output=$(./parallel-packed-csr -delete -threads=$t -update_file=../data/update_files/deletions.txt | sed '/Elapsed/!d' | sed -n '0~2p' | sed 's/Elapsed wall clock time: //g')
-    line="${line},${output}"
+    delete="${delete},${output}"
   done
+  avg=$(echo "$delete" | awk '{l=split($0,a,","); s=0; for (i in a)s+=a[i]; print s/(l-1);}')
+  delete="${delete},${avg}"
+
+  line="${insert}${delete}"
 
   echo "$line" | sed -e "s/^/$t/" | tee -a $outFile
 done
