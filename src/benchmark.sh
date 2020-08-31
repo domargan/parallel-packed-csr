1c60b18028801d53ad98d011050bc0036329af61
Domagoj Margan <margan.domagoj@gmail.com>
2020-08-31 16:02:02 +0100

    Script refactoring and improvements

--- src/benchmark.sh
+++ src/benchmark.sh
@@ -3,36 +3,67 @@
 
 ######################################
 
-# Define project paths
-PPCSR_PATH="$HOME/parallel-packed-csr"
-PPCSR_EXEC="$PPCSR_PATH/build/parallel-packed-csr"
+# Pass the benchmark config file path as a first script argument
+BENCHMARK_CONFIG_FILE="$1"
 
-# Define input files
-PPCSR_CORE_GRAPH_FILE="$1"
-PPCSR_INSERTIONS_FILE="$2"
-PPCSR_DELETIONS_FILE="$3"
+# The config file should contain and define the following variables:
+
+# Machine and dataset info for plotting:
+# MACHINE_NAME              -> Name of the testbed machine
+# DATASET_NAME              -> Dataset alias
+
+# Program and data input file paths:
+# PPCSR_EXEC                -> program binary file
+# PPCSR_CORE_GRAPH_FILE     -> core graph edgelist file
+# PPCSR_INSERTIONS_FILE     -> insertions update file
+# PPCSR_DELETIONS_FILE      -> deletions update file
+
+# Experiment parameters:
+# REPETITIONS               -> number of times to repeat the benchmark; integer
+# CORES                     -> number of cores to utilise in the scaling benchmark; array of integers
+# NUMA_BOUNDS               -> number of cores that mark end of NUMA domain boundaries; array of integers
+# SIZE                      -> number of edges that will be read from the update file; integer
+
+source $BENCHMARK_CONFIG_FILE
+if [ ! -f "$PPCSR_EXEC" ] ||
+	 [ ! -f "$PPCSR_CORE_GRAPH_FILE" ] ||
+	 [ ! -f "$PPCSR_INSERTIONS_FILE" ] ||
+	 [ ! -f "$PPCSR_DELETIONS_FILE" ]; then
+  echo -e "All required input data files are not found.\n"
+  exit 0
+fi
 
 # Define output files
 TIME=$(date +%Y%m%d_%H%M%S)
-PPCSR_LOG="strong_scaling_measurements_$TIME.csv"
-PPCSR_PLOT_DATA="strong_scaling_measurements_$TIME.dat"
-PPCSR_PLOT_FILE=$(mktemp gnuplot.pXXX)
-PPCSR_PDF_PLOT_FILE=ppcsr_strong_scaling_plot
+PPCSR_BENCHMARK_LOG="${MACHINE_NAME}_strong_scaling_log_${TIME}.txt"
+PPCSR_CSV_DATA="${MACHINE_NAME}_strong_scaling_${TIME}.csv"
+PPCSR_PLOT_DATA="${MACHINE_NAME}_strong_scaling_${TIME}.dat"
+PPCSR_PDF_PLOT_FILE="${MACHINE_NAME}_strong_scaling_${TIME}"
 
-# Define experiment parameters
-REPETITIONS=2
-CORES=(1 5 10 15 20)
-NUMA_BOUNDS=(10)
+# Write everyting to log file
+: > $PPCSR_BENCHMARK_LOG
+exec 2> >(tee -a $PPCSR_BENCHMARK_LOG >&2) > >(tee -a $PPCSR_BENCHMARK_LOG)
 
-SIZE=1000000
 
 ######################################
 
+echo "######################################"
+echo "Starting benchmark: strong scaling"
+
+echo "Testbed machine: $MACHINE_NAME"
+echo "Dataset: $DATASET_NAME"
 echo "Core graph file: $PPCSR_CORE_GRAPH_FILE"
 echo "Edge insertions file: $PPCSR_INSERTIONS_FILE"
 echo "Edge deletions file: $PPCSR_DELETIONS_FILE"
+echo "Repetitions: $REPETITIONS"
+echo "#cores: ${CORES[*]}"
+echo "NUMA domain boundaries: ${NUMA_BOUNDS[*]}"
+echo "Update batch size: $SIZE"
+echo -e "######################################\n"
 
 ######################################
+
+echo -e "[START]\t Starting computations...\n"
 
 # Write headers to CSV log
 header="#CORES"
@@ -49,48 +80,58 @@ writeHeader "DEL_PPPCSR"
 writeHeader "INS_PPPCSR_NUMA"
 writeHeader "DEL_PPPCSR_NUMA"
 
-echo "$header" | tee $PPCSR_LOG
+echo "$header" >>$PPCSR_CSV_DATA
 
 # Run the scaling experiment and write measures to the CSV log
 for core in ${CORES[@]}; do
-  line=""
+  csv=""
   dat=""
   for v in -ppcsr -pppcsr -pppcsrnuma; do
     insert=""
-    for ((r = 0; r < REPETITIONS; r++)); do
+    for ((r = 1; r <= REPETITIONS; r++)); do
+      echo -e "[START]\t ${v:1} edge insertions: Executing repetition #$r on $core cores..."
       output=$($PPCSR_EXEC -threads=$core $v -size=$SIZE -core_graph=$PPCSR_CORE_GRAPH_FILE -update_file=$PPCSR_INSERTIONS_FILE | sed '/Elapsed/!d' | sed -n '0~2p' | sed 's/Elapsed wall clock time: //g')
+      echo -e "[END]  \t ${v:1} edge insertions: Finished repetition #$r on $core cores.\n"
       insert="${insert},${output}"
     done
     avg_insert=$(echo "$insert" | awk '{l=split($0,a,","); s=0; for (i in a)s+=a[i]; print s/(l-1);}')
     insert="${insert},${avg_insert}"
 
     delete=""
-    for ((r = 0; r < REPETITIONS; r++)); do
+    for ((r = 1; r <= REPETITIONS; r++)); do
+      echo -e "[START]\t ${v:1} edge deletions: Executing repetition #$r on $core cores..."
       output=$($PPCSR_EXEC -delete -threads=$core $v -size=$SIZE -core_graph=$PPCSR_CORE_GRAPH_FILE -update_file=$PPCSR_DELETIONS_FILE | sed '/Elapsed/!d' | sed -n '0~2p' | sed 's/Elapsed wall clock time: //g')
+      echo -e "[END]  \t ${v:1} edge deletions: Finished repetition #$r on $core cores.\n"
       delete="${delete},${output}"
     done
     avg_delete=$(echo "$delete" | awk '{l=split($0,a,","); s=0; for (i in a)s+=a[i]; print s/(l-1);}')
     delete="${delete},${avg_delete}"
 
-    line="${line}${insert}${delete}"
+    csv="${csv}${insert}${delete}"
     dat="${dat}${avg_insert} ${avg_delete} "
-
   done
 
-  echo "$line" | sed -e "s/^/$core/" | tee -a $PPCSR_LOG
+  echo "$csv" | sed -e "s/^/$core/" >>$PPCSR_CSV_DATA
   echo $core $dat >>$PPCSR_PLOT_DATA
 done
 
+echo -e "[END]  \t Computations finished.\n"
 
 ######################################
 
 # Create the plot
+
+echo -e "[START]\t Starting data plotting...\n"
+
+PPCSR_PLOT_FILE=$(mktemp gnuplot.pXXX)
+
 XLABEL="#cores"
 YLABEL="CPU time (ms)"
 
 cat <<EOF >$PPCSR_PLOT_FILE
 set term pdf font ", 12"
 set output "${PPCSR_PDF_PLOT_FILE}.pdf"
+set title "Machine: $MACHINE_NAME \t Dataset: $DATASET_NAME \t #Updates: $SIZE"
 set xlabel "${XLABEL}"
 set ylabel "${YLABEL}" offset 1.5
 set size ratio 0.5
@@ -109,7 +150,7 @@ done
 
 cat <<EOF >>$PPCSR_PLOT_FILE
 #set ytics nomirror
-set key left top
+set key right top
 set key font ",12"
 
 set style line 1 lt 1 lc rgb "blue" lw 1 pt 5 ps 0.5
@@ -133,3 +174,9 @@ EOF
 gnuplot $PPCSR_PLOT_FILE
 rm $PPCSR_PLOT_FILE
 pdfcrop --margins "0 0 0 0" --clip ${PPCSR_PDF_PLOT_FILE}.pdf ${PPCSR_PDF_PLOT_FILE}.pdf &>/dev/null
+
+echo -e "[END]  \t Plotting finished.\n"
+
+echo "Exiting benchmark."
+
+exit 0
